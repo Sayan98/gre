086e2fc9b3f47a90e359554b0610fdf576e2d080
Sayan Goswami <goswami.sayan47@gmail.com>
2018-12-23 17:53:56 +0530

    Update setup.py
    Add metadata

--- setup.py
+++ setup.py
@@ -5,8 +5,18 @@ from setuptools import setup, find_packages
 setup(
     name='gre',
     version='0.1',
-    author="Sayan Goswami",
+    description='A handy helper to manage GRE Vocab preparation',
+    author='Sayan Goswami',
     author_email='<goswami.sayan47@gmail.com>',
+    long_description='README.md',
+    long_description_content_type='text/markdown',
+    url='https://github.com/Sayan98/gre',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
